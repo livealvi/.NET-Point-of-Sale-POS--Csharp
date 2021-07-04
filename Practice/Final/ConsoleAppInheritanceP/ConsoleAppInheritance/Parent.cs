850a848a3c2b57b9bf189212d8df4308c57299b7
livealvi <livealvi@gmail.com>
2021-07-05 02:40:43 +0600

    Mid & Final - Folder Create

---
+++ Practice/Final/ConsoleAppInheritanceP/ConsoleAppInheritance/Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInheritance
+{
+    public class Parent
+    {
+        internal Parent()
+        {
+            Console.WriteLine("Parent Called");
+        }
+    }
+}
