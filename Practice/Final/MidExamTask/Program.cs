0421fa7eb51c4861a04c9b110edd85d5f2144a14
livealvi <livealvi@gmail.com>
2021-07-12 02:55:54 +0600

    serach & deleted - implement

--- Practice/Final/MidExamTask/Program.cs
+++ Practice/Final/MidExamTask/Program.cs
@@ -11,15 +11,13 @@ namespace MidExamTask
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("\n\n======= Welcome to Employees Management System =======\n");
-
-            Employee[] emp = new Employee[4];
+            //Employee[] emp = new Employee[4];
             //emp[0] = new Employee("FirstName LastName",  "Unknown", 0000000, "Employee", new EmployeeInfo(new EmpResidenceInfo("Blank", "Blank", "01000000000"), new Date(0000, "Blank", 00), new Date(0000, "Blank", 00)));
-            emp[0] = new HRManager("Alvi Hasan",  "B+", 75000, "HR Manager", new EmployeeInfo(new EmpResidenceInfo("Kowtali", "Jessore", "01900000000"), new Date(1997, "OCT", 25), new Date(2021, "Jan", 11)), 5000);
-            emp[1] = new ITExecutive("Hasan Mahamud",  "B+", 55000, "IT Executive", new EmployeeInfo(new EmpResidenceInfo("Vatara", "Dhaka", "01700000000"), new Date(1998, "SEP", 20), new Date(2021, "Jan", 12)), 4500);
-            emp[2] = new MarketAnalyst("Rifat Karim",  "AB+", 45000, "Market Analyst", new EmployeeInfo(new EmpResidenceInfo("Khulna", "Khulna", "01800000000"), new Date(1999, "AUG", 10), new Date(2021, "Jan", 13)), 3500);
-            emp[3] = new PartTimer("Nobel Khan", "A+", 35000, "Part Timer", new EmployeeInfo(new EmpResidenceInfo("Badda", "Dhaka", "01500000000"), new Date(2000, "JUN", 07), new Date(2021, "Jan", 14)));
+            //emp[0] = new HRManager("Alvi Hasan",  "B+", 75000, "HR Manager", new EmployeeInfo(new EmpResidenceInfo("Kowtali", "Jessore", "01900000000"), new Date(1997, "OCT", 25), new Date(2021, "Jan", 11)), 5000);
+            //emp[1] = new ITExecutive("Hasan Mahamud",  "B+", 55000, "IT Executive", new EmployeeInfo(new EmpResidenceInfo("Vatara", "Dhaka", "01700000000"), new Date(1998, "SEP", 20), new Date(2021, "Jan", 12)), 4500);
+            //emp[2] = new MarketAnalyst("Rifat Karim",  "AB+", 45000, "Market Analyst", new EmployeeInfo(new EmpResidenceInfo("Khulna", "Khulna", "01800000000"), new Date(1999, "AUG", 10), new Date(2021, "Jan", 13)), 3500);
+            //emp[3] = new PartTimer("Nobel Khan", "A+", 35000, "Part Timer", new EmployeeInfo(new EmpResidenceInfo("Badda", "Dhaka", "01500000000"), new Date(2000, "JUN", 07), new Date(2021, "Jan", 14)));
 
             //byte i = 0;
             //while (i < emp.Length)
@@ -31,13 +29,15 @@ namespace MidExamTask
 
             //Employee e;
 
-            XYZCompany company = new XYZCompany();
-            company.AddEmployee(emp[0]);
-            company.AddEmployee(emp[1]);
-            company.AddEmployee(emp[2]);
-            company.AddEmployee(emp[3]);
-            company.ShowAll();
-
+            int w;
+            //XYZCompany company = new XYZCompany();
+            XYZCompany.AddEmployee(new HRManager("Alvi Hasan", "B+", 75000, "HR Manager", new EmployeeInfo(new EmpResidenceInfo("Kowtali", "Jessore", "01900000000"), new Date(1997, "OCT", 25), new Date(2021, "Jan", 11)), 5000));
+            XYZCompany.AddEmployee(new ITExecutive("Hasan Mahamud", "B+", 55000, "IT Executive", new EmployeeInfo(new EmpResidenceInfo("Vatara", "Dhaka", "01700000000"), new Date(1998, "SEP", 20), new Date(2021, "Jan", 12)), 4500));
+            XYZCompany.AddEmployee(new MarketAnalyst("Rifat Karim", "AB+", 45000, "Market Analyst", new EmployeeInfo(new EmpResidenceInfo("Khulna", "Khulna", "01800000000"), new Date(1999, "AUG", 10), new Date(2021, "Jan", 13)), 3500));
+            XYZCompany.AddEmployee(new PartTimer("Nobel Khan", "A+", 35000, "Part Timer", new EmployeeInfo(new EmpResidenceInfo("Badda", "Dhaka", "01500000000"), new Date(2000, "JUN", 07), new Date(2021, "Jan", 14))));
+            XYZCompany.ShowAll();
+            //XYZCompany.Search("E-1-HRM", out w);
+            //XYZCompany.DeleteEmployee("E-1-HRM");
 
         }
     }
--- Practice/Final/MidExamTask/XYZCompany.cs
+++ Practice/Final/MidExamTask/XYZCompany.cs
@@ -6,26 +6,74 @@ using System.Threading.Tasks;
 
 namespace MidExamTask.Employees
 {
-    internal class XYZCompany
+    internal static class XYZCompany
     {
+        private static Employee[] employeesList = new Employee[3000];
         private static int count = 0;
-        private Employee[] employeesList = new Employee[3000];
 
-        internal void AddEmployee(Employee employee)
+        internal static void AddEmployee(Employee employee)
         {
-            this.employeesList[count] = employee;
+            employeesList[count] = employee;
             count++;
         }
-        internal void ShowAll()
+
+        internal static void ShowAll()
         {
             int index = 0;
-
             while (index < count)
             {
+                if (employeesList[index] == null)
+                {
+                    index++;
+                    continue;
+                }
                 employeesList[index].ShowEmployeeInfo(); // runtime polymorphism .ShowEmployeeInfo()
                 Console.WriteLine();
                 index++;
             }
+        }
+
+        internal static bool Search(string key, out int counter)
+        {
+            bool found = false;
+
+            int index = 0;
+            while (index < count)
+            {
+                if(key.Equals(employeesList[index].EmpID))
+                {
+                    Console.WriteLine("\n\n\t-------- Employee Search ------------\n");
+                    Console.Write("\tEmployee Found");
+                    employeesList[index].ShowEmployeeInfo();
+                    //Console.WriteLine("\n\t-------------------------------------\n");
+                    Console.WriteLine();
+                    found = true;
+                    //break;
+                    counter = index;
+                    return found;
+                }
+                index++;
+            }
+            if (!found) 
+                Console.WriteLine("\n\n\t-------- Employee Search ------------\n");
+            Console.WriteLine("\tNot Found\n");
+            Console.WriteLine("\t-------------------------------------\n");
+            counter = -1;
+                return found;
+        }
+
+        internal static void DeleteEmployee(string key)
+        {
+            int indexNumber;
+            bool decision = Search(key, out indexNumber);
+            if (decision)
+            {
+                Console.WriteLine("\t-------------------------------------\n");
+                employeesList[indexNumber] = null;
+                Console.WriteLine("\tData Deleted Succeed.\n");
+                Console.WriteLine("\t-------------------------------------\n");
+            }
+            ShowAll();
         }
 
     }
