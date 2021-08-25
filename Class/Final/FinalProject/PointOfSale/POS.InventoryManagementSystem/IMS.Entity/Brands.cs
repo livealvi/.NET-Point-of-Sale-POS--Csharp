cd07aaeffb222e94cbf9028e60b6f4e9df118a1b
livealvi <livealvi@gmail.com>
2021-08-26 02:44:56 +0600

    Brand Enitiy - CleanUp & Products_Categories_Map.cs - Delete

--- Class/Final/FinalProject/PointOfSale/POS.InventoryManagementSystem/IMS.Entity/Brands.cs
+++ Class/Final/FinalProject/PointOfSale/POS.InventoryManagementSystem/IMS.Entity/Brands.cs
@@ -11,17 +11,15 @@ namespace IMS.Entity
         public int           BrandId         { get; set; }
         public string        BrandTag       { get; set; }
         public string        BrandName       { get; set; }
-        public Nullable<int> VendorId        { get; set; }
+        public int VendorId        { get; set; }
         public string        BrandDescription{ get; set; }
         public string        BrandStatus     { get; set; }
         public byte[]        BrandImage      { get; set; }
 
-        //outside
+        //outside - Vendor
         public string VendorName{get; set;} 
 
         public virtual Vendors Vendor{ get; set; }
         public virtual ICollection<Products> Products{ get; set; }
-        public virtual ICollection<Products_Categories_Map> Products_Categories_Map{ get; set; }
-
     }
 }
